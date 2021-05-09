d3c61c287f1e850027d1cd9b080cc920785687a9
se-prashant <prashant791singh@gmail.com>
2021-05-09 13:09:21 +0530

    connected mongodb-atlas

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname +"/date.js");
+const mongoose = require("mongoose");
+
+var _ = require('lodash');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,36 +14,151 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const tasks = [];
-const workItems = [];
-
-app.get("/",function(req,res){
-  let day  = date.getDate();
-  res.render("list",{listTitle:day,newTask:tasks});
-    // res.send("Work Day");
+// mongoose.connect("mongodb://localhost:27017/todoListDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false
+// });
+mongoose.connect("mongodb+srv://prashant-recs:abcdefij@cluster0.pjgbg.mongodb.net/todoListDB",{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useFindAndModify:false
 });
 
-app.get("/work",function(req,res){
-  res.render("list",{listTitle:"Work List", newTask:workItems})
+const itemsSchema = mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todoList :)"
+});
+
+
+const item2 = new Item({
+  name: "Hit the + button to add a new task"
+});
+
+const item3 = new Item({
+  name: "click on checkbox <-- to delete an item."
+});
+const defaultItems = [item1, item2,item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+const List = mongoose.model("List", listSchema);
+
+
+app.get("/", function(req, res) {
+  Item.find({}, function(err, todoItems) {
+    if (todoItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          // console.log("successfull", defaultItems);
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newTask: todoItems
+      });
+    }
+  });
+});
+
+app.get("/:customlistName", function(req, res) {
+  const customlistName = _.capitalize(req.params.customlistName);
+  // console.log(customlistName);
+  List.findOne({
+    name: customlistName
+  }, function(err, foundList) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundList) {
+        //Show an existing list
+
+        res.render("list", {
+          listTitle: foundList.name,
+          newTask: foundList.items
+        });
+      } else {
+        // Create a new listT
+        const list = new List({
+          name: customlistName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customlistName);
+      }
+    }
+  });
+
+
+
 })
 
-app.get("/about",function(req,res){
+app.get("/about", function(req, res) {
   res.render("about");
 })
 
-app.post("/",function(req,res){
-  let newTask = req.body.newTask;
-  if(req.body.title === "Work List"){
-    workItems.push(newTask);
-    res.redirect("/work");
+app.post("/delete", function(req, res) {
+  const item_id = req.body.checkbox;
+  const listName = req.body.listName;
+  // console.log(item_id," ",listName);
+  if (listName == "Today") {
+    Item.findByIdAndRemove(item_id, function(err) {
+      if (!err) {
+        console.log("Successfully Deleted");
+        res.redirect("/");
+      }
+    });
   }else{
-    tasks.push(newTask)
-    res.redirect("/");
+    List.findOneAndUpdate({name:listName},
+      {$pull:{items:{_id:item_id}}},
+      function(err,foundList){
+      if(!err){
+        res.redirect("/"+listName);
+      }
+    });
   }
-  // res.render("list",{task:task});
+})
+
+app.post("/", function(req, res) {
+  const listName = req.body.title;
+  const taskName = req.body.newTask;
+  const newTask = new Item({
+    name: req.body.newTask
+  });
+  if (listName == "Today") {
+    newTask.save();
+    res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      if (err) {
+        console.log(err);
+      } else {
+        const task = {
+          name: taskName
+        };
+        foundList.items.push(task);
+        foundList.save();
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
 });
 
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("server started on port-3000");
 })
